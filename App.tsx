3d1fe8f1d08a7181774c3aaf21c3c6cf95ab6fd7
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 17:38:15 +0400

    Implement loading state in App component with Pressable for login action, enhancing user interaction and visual feedback.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
-import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Pressable, Text, View } from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = () => {
+    setLoading(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <ActivityIndicator size="large" />
+    <View
+      style={{
+        padding: 50,
+      }}
+    >
+      <Pressable onPress={handleLogin}>
+        {loading ? <ActivityIndicator /> : <Text>Login</Text>}
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
