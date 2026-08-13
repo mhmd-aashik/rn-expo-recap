48953501c2adab5daef1e513ae725a76cda4b6ed
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 16:43:25 +0400

    Update App component to include two new styled views, "First" and "Second", enhancing layout and visual structure.

--- App.tsx
+++ App.tsx
@@ -3,7 +3,13 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native</Text>
+      <View style={styles.first}>
+        <Text>First</Text>
+      </View>
+
+      <View style={styles.second}>
+        <Text>Second</Text>
+      </View>
     </View>
   );
 }
@@ -11,6 +17,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  first: {
+    flex: 1,
+    backgroundColor: "lightgreen",
+  },
+
+  second: {
+    flex: 2,
     backgroundColor: "lightblue",
   },
 });
