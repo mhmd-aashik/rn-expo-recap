0908ecf3874efbde262611bd8de961f036ff3c36
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 17:18:48 +0400

    Refactor App component to replace static greeting text with a FlatList displaying user names, enhancing dynamic content rendering and layout.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { Image, ScrollView, Text, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const users = [
+  { id: "1", name: "John" },
+  { id: "2", name: "David" },
+  { id: "3", name: "Sarah" },
+];
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Text>Hello Expo</Text>
+        <FlatList
+          data={users}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Text style={styles.item}>{item.name}</Text>
+          )}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -15,5 +27,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+  },
+
+  item: {
+    padding: 15,
   },
 });
