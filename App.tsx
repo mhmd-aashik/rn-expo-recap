dd0def2b9dd4ce98c3f5b044532297cc359bd077
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 16:45:43 +0400

    Update App component styles to center content by adding justifyContent property and commenting out flexDirection.

--- App.tsx
+++ App.tsx
@@ -21,7 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
+    // flexDirection: "column",
+    justifyContent: "center",
   },
 
   box: {
