0f8cae6ec4935422da2c93ac97b760234046386e
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 16:38:25 +0400

    Refactor App component to remove Profile and Settings sections, replacing them with a single text element and updating styles for improved layout and background color.

--- App.tsx
+++ App.tsx
@@ -3,24 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Profile</Text>
-      </View>
-
-      <View style={styles.container}>
-        <Text style={styles.title}>Settings</Text>
-      </View>
+      <Text>React Native</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-  },
-
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
+    flex: 1,
+    backgroundColor: "lightblue",
   },
 });
