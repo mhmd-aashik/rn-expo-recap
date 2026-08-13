44334931f394f7e110c618df421bb3ae6d5f69dc
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 17:08:14 +0400

    Refactor App component to use SafeAreaProvider and SafeAreaView, simplifying layout and updating content to display a greeting message.

--- App.tsx
+++ App.tsx
@@ -1,48 +1,19 @@
 import { Image, ScrollView, Text, StyleSheet, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.content}
-        showsVerticalScrollIndicator={false}
-      >
-        <View style={styles.box}>
-          <Text>Box 1</Text>
-        </View>
-
-        <View style={styles.box}>
-          <Text>Box 2</Text>
-        </View>
-
-        <View style={styles.box}>
-          <Text>Box 3</Text>
-        </View>
-
-        <View style={styles.box}>
-          <Text>Box 4</Text>
-        </View>
-      </ScrollView>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text>Hello Expo</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-
-  box: {
-    height: 250,
-    backgroundColor: "lightblue",
-    margin: 10,
     padding: 20,
-    borderRadius: 20,
-  },
-
-  content: {
-    padding: 20,
-    gap: 10,
   },
 });
