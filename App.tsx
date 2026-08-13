b6ffe5963f28fbb25d6f6fdbbf6e0acd458c7b0c
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 17:28:06 +0400

    Refactor App component to use KeyboardAvoidingView for better keyboard handling, update styles for improved layout, and remove unused state management.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
-
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState("");
-
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
       <TextInput
-        value={name}
-        onChangeText={setName}
-        placeholder="Enter your name"
         style={styles.input}
+        placeholder="Enter your name"
       />
-
-      <Text>Hello {name}</Text>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 80,
-    padding: 40,
+    justifyContent: 'flex-end',
+    padding: 20,
   },
 
   input: {
     borderWidth: 1,
-    borderColor: "#999",
     padding: 12,
     borderRadius: 8,
   },
