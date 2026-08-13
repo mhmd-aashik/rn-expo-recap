3485cf312f58ca62af7290284827edbda3dd7aaf
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-13 17:46:46 +0400

    Refactor App component to display window dimensions instead of platform-specific greeting, simplifying layout and enhancing responsiveness.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Text, useWindowDimensions, View } from "react-native";
 
 export default function App() {
-  const message = Platform.OS === "ios" ? "hello ios" : "hello android";
-  return (
-    <View style={styles.container}>
-      <Text>Running on: {Platform.OS}</Text>
+  const { width, height } = useWindowDimensions();
 
-      <Text>{message}</Text>
+  return (
+    <View>
+      <Text>Width: {width}</Text>
+      <Text>Height: {height}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-});
